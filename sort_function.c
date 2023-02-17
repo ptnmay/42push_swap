02a920fae36a71ea983770461795637ec9b1bb31
Pittinan <105956506+ptnmay@users.noreply.github.com>
2023-02-18 03:28:44 +0700

    p'palm help rotate

--- sort_function.c
+++ sort_function.c
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 04:34:59 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/02/18 03:08:24 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/02/18 03:24:29 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_stack *switch_it(t_stack *ab)
 {
     t_stack *clone;
-    
+
     clone = ab->next;
     ab->next = clone->next;
     clone->next = ab;
@@ -26,15 +26,16 @@ t_stack *rotate_it(t_stack *ab)
 {
     t_stack *clone;
     t_stack *end;
-    
+
     clone = ab;
     ab = ab->next;
     end = set_last(ab);
     end->next = clone;
+    clone->next = NULL;
     printf("\nab in rotate = [%d]\n", ab->numero);
     printf("ab next in rotate = [%d]\n", ab->next->numero);
     printf("ab next next in rotate = [%d]\n", ab->next->next->numero);
-    ab->next->next->next = NULL;
+    //ab->next->next->next = NULL;
     // printf("ab next next next in rotate = [%d]\n", ab->next->next->next->numero);
     return(ab);
 }
